714117c2e12fe663ba0c632211a1fff3faec1f15
mildrette <mildredfonka1@gmail.com>
2025-02-14 13:34:34 +0100

    added download sectiona and foote

--- src/Component/Download.js
+++ src/Component/Download.js
@@ -1,50 +1,47 @@
 import React from 'react';
-import '../App.css';
 
-const DownloadDeku = () => {
+const DownloadSection = () => {
   return (
-    <section id="download-deku" className="deku-download-section">
-      <div className="deku-download-text">
-        <div className="deku-download-text__tagline">Stay Connected with Deku SMS</div>
-        <br />
-        <div className="deku-download-text__cta">Download the App</div>
-      </div>
-
-      <div className="deku-download-links">
-        <div className="deku-download-buttons">
-          <a
-            className="deku-download-button deku-download-playstore"
-            href="https://play.google.com/store/apps/details?id=bbc.mobile.news.ww&hl=en"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <img 
-              src="https://play.google.com/intl/en_us/badges/images/generic/en-play-badge.png"    
-              alt="Get it on Google Play"
-              className="deku-download__play-store"
-              width="200"
-              height="auto"
-            />
-          </a>
-
-          <a
-            className="deku-download-button deku-download-fdroid"
-            href="https://itunes.apple.com/us/app/bbc-news/id364147881?mt=8"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <img 
-              src="https://fdroid.gitlab.io/artwork/badge/get-it-on.png" 
-              alt="Get it on F-Droid" 
-              className="deku-download__fdroid"
-              width="200"
-              height="auto"
-            />
-          </a>
+    <section id="download" className="py-5 bg-gradient text-light">
+      <div className="container text-center">
+        <h2 className="display-3 font-weight-bold mb-4">Get DekuSMS</h2>
+        <p className="lead mb-5">
+          Join thousands of users and download DekuSMS today! Available on your favorite platforms.
+        </p>
+        <div className="row justify-content-center">
+          <div className="col-lg-3 col-md-4 col-sm-6 mb-4">
+            <a
+              href="https://play.google.com/store/apps/details?id=com.dekusms"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <button className="btn btn-lg btn-outline-light w-100 shadow-lg p-3">
+                <i className="fab fa-google-play mr-2"></i> Google Play
+              </button>
+            </a>
+          </div>
+          <div className="col-lg-3 col-md-4 col-sm-6 mb-4">
+            <a
+              href="https://f-droid.org/packages/com.dekusms"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <button className="btn btn-lg btn-outline-light w-100 shadow-lg p-3">
+                <i className="fab fa-android mr-2"></i> F-Droid
+              </button>
+            </a>
+          </div>
+          <div className="col-lg-3 col-md-4 col-sm-6 mb-4">
+            <a href="https://example.com/dekusms.apk" download>
+              <button className="btn btn-lg btn-outline-light w-100 shadow-lg p-3">
+                <i className="fas fa-download mr-2"></i> Download APK
+              </button>
+            </a>
+          </div>
         </div>
       </div>
     </section>
   );
 };
 
-export default DownloadDeku;
+export default DownloadSection;
--- src/Component/Footer.js
+++ src/Component/Footer.js
@@ -4,27 +4,42 @@ import "../App.css";
 
 const Footer = () => {
   return (
-    <footer className="footer">
-      <div className="footer-content">
-        {/* Footer Links */}
-        <div className="footer-row">
-          <ul>
-            <li><a href="#">Contact Us</a></li>
-            <li><a href="#">Privacy Policy</a></li>
-            <li><a href="#">Terms & Conditions</a></li>
+    <footer className="footer py-5 bg-dark text-white">
+      <div className="container">
+        <div className="d-flex justify-content-between align-items-center mb-4">
+          <img
+            src="DekuSMS-Dark Theme.png"
+            alt="Logo"
+            className="img-fluid"
+            style={{ height: "30px" }}
+          />
+
+          <ul className="list-inline mb-0">
+            <li className="list-inline-item mx-3">
+              <a href="https://smswithoutborders.com/privacy-policy" className="footer-link text-white">
+                Privacy Policy
+              </a>
+            </li>
+            <li className="list-inline-item mx-3">
+              <a href="https://docs.smswithoutborders.com/" className="footer-link text-white">
+                Support
+              </a>
+            </li>
           </ul>
         </div>
-
-        {/* Social Icons */}
-        <div className="footer-row">
+        
+        <hr className="border-light" />
+        
+        <div className="footer-social-icons text-center mb-4">
           <a
-            href="https://github.com/dekusms/Deku-SMS-Android"
+            href="https://github.com/dekusms/DekuSMS-Android"
             target="_blank"
             rel="noopener noreferrer"
             aria-label="GitHub"
             title="Visit our GitHub repository"
+            className="social-icon mx-3 text-white"
           >
-            <FaGithub />
+            <FaGithub size={24} />
           </a>
           <a
             href="https://twitter.com/deku_sms"
@@ -32,8 +47,9 @@ const Footer = () => {
             rel="noopener noreferrer"
             aria-label="Twitter"
             title="Follow us on Twitter"
+            className="social-icon mx-3 text-white"
           >
-            <FaTwitter />
+            <FaTwitter size={24} />
           </a>
           <a
             href="https://t.me/deku_sms"
@@ -41,14 +57,14 @@ const Footer = () => {
             rel="noopener noreferrer"
             aria-label="Telegram"
             title="Join us on Telegram"
+            className="social-icon mx-3 text-white"
           >
-            <FaTelegramPlane />
+            <FaTelegramPlane size={24} />
           </a>
         </div>
-
-        {/* Footer Copyright */}
-        <div className="footer-row">
-          © DekuSMS || Afkanerd
+        
+        <div className="footer-copyright text-center mt-4">
+          <small>© 2025 DekuSMS || Afkanerd</small>
         </div>
       </div>
     </footer>
